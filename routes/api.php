03cdf54a0c7585dbaaa0cb4d801d7db2c1f98af7
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-05-24 00:46:58 -0600

    setting up CRUD functionality

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\ArticlesController;
+use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['as' => 'api.'], function() {
+    Orion::resource('articles', ArticlesController::class);
+});
